11d92ebff36aaca0218060362526abe3c4886db8
YuriNikolai <yurinikolai00@gmail.com>
2022-02-16 22:07:16 -0300

    hex

--- scripts/InputController.gd
+++ scripts/InputController.gd
@@ -5,3 +5,4 @@ onready var weapon = get_parent().get_node("Weapon")
 func _process(_delta):
 	if Input.is_action_pressed("fire") and weapon.can_fire:
 		weapon.fire()
+		owner.get_node("Plane").hit(100)
