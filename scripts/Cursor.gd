35caf84dea98a89cf72f613a8f7e9685cbb3dda1
YuriNikolai <yurinikolai00@gmail.com>
2022-02-23 01:18:25 -0300

    cursor change logic

--- scripts/Cursor.gd
+++ scripts/Cursor.gd
@@ -1,12 +1,22 @@
 extends Node
 
 
-# Load the custom images for the mouse cursor.
+# Load the custom images for the mouse cursor
 var flare = load("res://gfx/gui/cursor_flare.png")
 var laser = load("res://gfx/gui/cursor_laser.png")
+onready var weapon = get_parent().get_node("Weapon")
 
 func _ready():
+	
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW) # might not be necessary; confirms the default
 
 	Input.set_custom_mouse_cursor(flare, Input.CURSOR_ARROW, Vector2 (12,12))
 
 	Input.set_custom_mouse_cursor(laser, Input.CURSOR_CROSS, Vector2 (12,12))
+	
+func _process(_delta):
+	
+	if weapon.is_hitscan == false: # circle (arrow) for flare, cross for rifle
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_CROSS)
--- scripts/Flare.gd
+++ scripts/Flare.gd
@@ -2,9 +2,9 @@ extends RigidBody2D
 
 onready var _animated_sprite = $AnimatedSprite
 
-#Make anim start on a consistent frame. Keep it at 8 FPS on the editor
+
 func _ready():
-	_animated_sprite.frame = 0
+	_animated_sprite.frame = 0 #Makes anim start on a consistent frame
 
 func _on_LifeTimer_timeout():
 	queue_free()
