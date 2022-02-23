d90dcec00d1d0374505297b62e6e7b35f8ec070e
YuriNikolai <yurinikolai00@gmail.com>
2022-02-23 00:52:34 -0300

    formatting credits, tweaks

--- scripts/InputController.gd
+++ scripts/InputController.gd
@@ -6,7 +6,7 @@ var AudioStreamer = preload("res://scenes/AudioStreamer.tscn")
 var sfx_eject = preload("res://sfx/eject.wav")
 var sfx_flare_load1 = preload("res://sfx/flare_load1.wav")
 var sfx_flare_load2 = preload("res://sfx/flare_load2.wav")
-var sfx_dry_fire = preload("res://sfx/dry_fire.wav")
+var sfx_trigger = preload("res://sfx/trigger.wav")
 
 var fired_this_click = false
 
@@ -19,7 +19,7 @@ func _process(_delta):
 	if Input.is_action_just_pressed("fire") and weapon.can_fire == false:
 		var sound = AudioStreamer.instance()
 		add_child(sound)
-		sound.play_sound(sfx_dry_fire)
+		sound.play_sound(sfx_trigger)
 	
 	if Input.is_action_just_released("fire") and fired_this_click:
 		var sound = AudioStreamer.instance()
