ab8668e914d9972b7af2cbde76923028cd91aa82
lmlask <luimlask@gmail.com>
2022-02-28 19:08:07 -0300

    fix, windows export settings

--- scripts/EndMenu.gd
+++ scripts/EndMenu.gd
@@ -12,21 +12,12 @@ func _ready():
 	#$HBoxContainer/DaySelectButton.grab_focus()
 	pass
 	
-func _on_DaySelectButton_pressed():
+func _on_QuitButton_pressed():
 	sound = AudioStreamer.instance()
 	add_child(sound)
 	sound.play_sound(sfx_navigation)
-	get_tree().change_scene("res://scenes/Menu.tscn")
-	
-func _on_QuitButton_pressed():
 	get_tree().quit()
 
-func _on_DaySelectButton_mouse_entered():
-	$HBoxContainer/DaySelectButton.modulate = Color(hover, hover, hover, 1)
-
-func _on_DaySelectButton_mouse_exited():
-	$HBoxContainer/DaySelectButton.modulate = Color(nhover, nhover, nhover, 1)
-	
 func _on_QuitButton_mouse_entered():
 	$HBoxContainer/QuitButton.modulate = Color(hover, hover, hover, 1)
 
