88608677b14e825d383f7b336faec9f9d6a74ca7
YuriNikolai <yurinikolai00@gmail.com>
2022-04-24 18:02:57 -0300

    organise, hit detection and particles overhaul

--- scripts/Flare.gd
+++ scripts/Flare.gd
@@ -4,7 +4,7 @@ var AudioStreamer = preload("res://scenes/AudioStreamer.tscn")
 var sfx_boom = preload("res://sfx/boom.wav")
 
 onready var _animated_sprite = $AnimatedSprite
-onready var raycast = $RayCast2D
+onready var raycast = [$RayCast2DLeft, $RayCast2DCenter, $RayCast2DRight]
 
 export var flare_damage = 5
 
@@ -15,13 +15,37 @@ func _ready():
 
 func _physics_process(delta):
 #	print(raycast.get_collider())
-	if raycast.is_colliding() and burned != true:
-		burned = true
-		raycast.get_collider().hit(flare_damage)
-		queue_free()
+	for i in range(0, len(raycast)-1):
+		if raycast[i].is_colliding() and burned != true:
+			burned = true
+			raycast[i].get_collider().hit(flare_damage)
+			spawn_hit_particles()
 
 func _on_LifeTimer_timeout():
 	queue_free()
 
-func hit(dmg):
-	pass
+func spawn_hit_particles():
+	var hit_particles_instance = CPUParticles2D.new()
+	hit_particles_instance.emitting = true
+	hit_particles_instance.one_shot = true
+	hit_particles_instance.amount = 512
+	hit_particles_instance.lifetime = 0.5
+	hit_particles_instance.explosiveness = 1
+	hit_particles_instance.randomness = 0.5
+	hit_particles_instance.lifetime_randomness = 10
+	hit_particles_instance.direction.x = 0
+	hit_particles_instance.spread = 152
+	hit_particles_instance.gravity.x = 0
+	hit_particles_instance.emission_shape = 1 #Sphere
+	hit_particles_instance.initial_velocity = 66.66
+	hit_particles_instance.linear_accel = 22.22
+	hit_particles_instance.linear_accel_random = 0.13
+	hit_particles_instance.radial_accel = 9.0
+	hit_particles_instance.damping = 43.22
+	hit_particles_instance.scale_amount = 1.5
+	hit_particles_instance.color = Color.orange
+	hit_particles_instance.z_index = 1
+	hit_particles_instance.modulate = Color(1.2, 1.2, 1.2, 1) #Glow
+	hit_particles_instance.position = Vector2(get_global_position().x, get_global_position().y)
+	get_tree().root.get_child(0).add_child(hit_particles_instance)
+	queue_free()
